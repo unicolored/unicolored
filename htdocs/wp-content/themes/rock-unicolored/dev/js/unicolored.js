6454c92d131da71c0ffc9aea2b173574dae48010
Gilles Hoarau <unicolored@gmail.com>
2014-12-14 06:15:54 +0100

    Mise en développement du site

---
+++ htdocs/wp-content/themes/rock-unicolored/dev/js/unicolored.js
@@ -0,0 +1,32 @@
+/*!
+unicolored */
+var unicolored = angular.module( 'unicolored', [ 'ngMaterial', 'ngAnimate', 'ngRoute', 'ngSanitize' ] );
+/*
+// LocalStorageModule //https://github.com/grevory/angular-local-storage
+An AngularJS module that gives you access to the browsers local storage with cookie fallback
+*/
+/*
+// angularFileUpload // https://github.com/nervgh/angular-file-upload
+Supports drag-n-drop upload, upload progress, validation filters and a file upload queue. It supports native HTML5 uploads, but degrades to a legacy iframe upload method for older browsers. Works with any server side platform which supports standard HTML form uploads.
+When files are selected or dropped into the component, one or more filters are applied. Files which pass all filters are added to the queue. When file is added to the queue, for him is created instance of {FileItem} and uploader options are copied into this object. After, items in the queue (FileItems) are ready for uploading.
+*/
+// CONFIG
+unicolored.config( function() {
+    'use strict';
+} );
+// RUN
+unicolored.run( function() {
+    'use strict';
+} );
+unicolored.controller( 'BonjourController', [ '$scope', '$http', function( $scope, $http ) {
+    'use strict';
+    $scope.world = 'Gilles';
+    $scope.articles = [];
+    var httpRequest = $http( {
+        method: 'GET',
+        url: '/wp-json/posts/',
+        //data: mockDataForThisTest
+    } ).success( function( data, status ) {
+        $scope.articles = data;
+    } );
+} ] );
